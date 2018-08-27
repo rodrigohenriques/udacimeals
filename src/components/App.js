fc5ab3a326f4ed8b35706da4c0a2f4be7350c362
Rodrigo Henriques <rsh.trabalho@gmail.com>
2018-08-27 16:49:53 -0300

    feat: Add combine reducers to handle food state

--- src/components/App.js
+++ src/components/App.js
@@ -12,16 +12,20 @@ class App extends Component {
   }
 }
 
-function mapStateToProps(calendar) {
+function mapStateToProps ({ food, calendar }) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
   return {
-    calendar: Object.keys(calendar).map(
-      (day) => (
-        {
-          day,
-          meals: { ...calendar[day] }
-        }
-      )
-    )
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? food[calendar[day][meal]]
+          : null
+
+        return meals
+      }, {})
+    })),
   }
 }
 
